e1d0c2febb5ba7ec8e6f44f301a945484807a2e6
doncibo <qajonesx47@gmail.com>
2020-08-31 17:21:14 -0500

    frontend auth redux reducer for login

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -1,14 +1,37 @@
 import Cookies from 'js-cookie';
 
+const SET_USER = 'auth/SET_USER';
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        user
+    };
+};
+
 export const login = (username, password) => {
-    return dispatch => {
+    return async dispatch => {
         const csrfToken = Cookies.get("XSRF-TOKEN");
-        const res = fetch('/api/session', {
+        const res = await fetch('/api/session', {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
                 "X-CSRF-TOKEN": csrfToken,
-            }
-        })
+            },
+            body: JSON.stringify({ username, password })
+        });
+        res.data = await res.json(); // will contain user info { user: {id, username, ...}}
+        if(res.ok){
+            dispatch(setUser(res.data.user));
+        }
+        return res;
+    }
+};
+
+export default function authReducer(state = {}, action) {
+    switch (action.type) {
+        case SET_USER:
+            return action.user;
+        default:
+            return state;
     }
 }
